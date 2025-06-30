6c4c82fa013c4c53a29e2f601f9ed2df4b026883
Alrenado <eugene1998@ukr.net>
2025-06-30 16:59:23 +0300

    feat : full header
    js body overflow lock when press burger
    add header overflow-y
    full adaptive

--- js/form.js
+++ js/form.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const burger = document.getElementById("burger");
     const menu = document.querySelector(".navbar__menu");
+    const body = document.body;
 
     burger.addEventListener("click", () => {
+        const isOpen = menu.classList.contains("open");
+
         burger.classList.toggle("open");
         menu.classList.toggle("open");
+
+        if (isOpen) {
+            body.classList.remove("no-scroll");
+        }else {
+            body.classList.add("no-scroll");
+        }
     });
 });
